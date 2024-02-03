075e7e1a9d5ac4df62399c9688e4e1ce68c783f6
Lutendo Mulaisi <61361486+ltnlutendo@users.noreply.github.com>
2024-02-03 18:15:42 +0200

    Add files via upload

---
+++ perf/__init__.py
@@ -0,0 +1,5 @@
+# perf/__init__.py
+
+from perf.py import SayHello, HowAreYou, Goodbye
+
+__all__ = ['SayHello', 'HowAreYou', 'Goodbye']
---
+++ perf/perf.py
@@ -0,0 +1,10 @@
+# perf/perf.py
+
+def SayHello():
+    return "Hello!"
+
+def HowAreYou():
+    return "I'm doing well, thank you!"
+
+def Goodbye():
+    return "Goodbye!"
